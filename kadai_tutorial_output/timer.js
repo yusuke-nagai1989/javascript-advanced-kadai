9e70e007f4f0f3e5f5ebd8a1ba88a2c50fe051ee
yusuke-nagai1989 <156492699+yusuke-nagai1989@users.noreply.github.com>
2024-02-22 22:15:54 +0900

    kadai_tutorial_outputのアップロードです

---
+++ kadai_tutorial_output/timer.js
@@ -0,0 +1,14 @@
+//カウントダウンタイマー
+const timer = () => {
+
+  // タイマー部分のHTML要素（p要素）を取得する
+  let time = count.textContent;
+
+  const id = setInterval;
+
+  // カウントダウンする
+  time--;
+  count.textContent = time;
+
+
+};
